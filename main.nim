9c1631afe37e13bb53d5836b4ebfafb1eb8edb42
Just-Jasmine <doyoulikejas64@gmail.com>
2023-10-24 16:04:18 -0400

    Added Pointer Logic And Comments

--- main.nim
+++ main.nim
@@ -1,8 +1,47 @@
 import terminal, strutils, random
 
+# adds the X or O to the grid
+proc xoPrint(ptrArray: array[3, ptr], xo: ptr, grid: ptr) =
+  case xo[]
+  of "X": xo[] = "\e[31mX\e[m"
+  of "O": xo[] = "\e[32mO\e[m"
+  grid[][ptrArray[1][]][ptrArray[2][]] = xo[]
+  return
+
+# Selects a the slot from the 3 by 3 array
+proc chooseSlot(ptrArray: array[3, ptr]) =
+  ptrArray[1][] = (ptrArray[0][] - 1) div 3
+  ptrArray[2][] = (ptrArray[0][] - 1) mod 3
+  return
+
+# Executes ai turn Logic
 proc aiMove() =
   return
 
+# Executes player turn Logic
+proc playerMove(ptrArray: array[3, ptr], draw: ptr, turns: ptr, grid: ptr) =
+  try:
+    echo("Enter slot number (1-9): ")
+    ptrArray[0][] = parseInt($getch())
+    chooseSlot(ptrArray)
+    return
+  except:
+    draw[] = false
+    turns[] += 1
+    return
+  try:
+    if grid[][ptrArray[1][]][ptrArray[2][]] != $ptrArray[0][]:
+      draw[] = false
+      turns[] += 1
+      echo("Slot taken. Choose another one: ")
+      return
+  except:
+    draw[] = false
+    turns[] += 1
+    echo("Please choose a number 1-9: ")
+    return
+
+# returns an initial 3 by 3 array
 proc initGrid(): array[3, array[3, string]] =
   var
     num = 1
@@ -21,6 +60,7 @@ proc initGrid(): array[3, array[3, string]] =
   
   return grid
 
+# uses for loops to draw a 3 by 3 array passed to it
 proc drawGrid(grid: array[3, array[3, string]]) =
   stdout.write("\e[2J\e[H")
 
@@ -35,15 +75,21 @@ proc drawGrid(grid: array[3, array[3, string]]) =
     if i < 2:
       echo("---------")
 
+# Main function
 proc ticTacToe() =
+  # Initializes variables
   var
     grid = initGrid()
-    moves = 9
+    turns = 9
     draw = true
     aiTurn = false
     aiEnabled = false
     xo: string
+    slot: int
+    row: int
+    col: int
 
+  # Welcomes the user and asks if they want ai enabled
   while (true):
     echo("Welcome to TicTacToe. Enable AI?(Y/N): ")
     try:
@@ -60,6 +106,7 @@ proc ticTacToe() =
       echo("Please enter Y or N: ")
       continue
 
+  # Asks the user what character they want to use, X or 0
   while (true):
     echo("X or O?: ")
     try:
@@ -76,51 +123,22 @@ proc ticTacToe() =
     except:
       echo("Please enter X or O: ")
       
-  for m in 1..moves:
+  # Loops for how many turns are left
+  for t in 1..turns:
+    # draws a grid if draw is true
     if draw: 
       drawGrid(grid)
     draw = true
-
+    
+    # Checks if it's the AI's turn, and executes if it isn't
     if (aiTurn != true):
-      echo("Enter slot number (1-9): ")
-
-      # Get Slot Position in grid
-      var
-          slot:int
-          row: int
-          col: int
-      try:
-          slot = parseInt($getch())
-          row = (slot - 1) div 3
-          col = (slot - 1) mod 3
-      except:
-          draw = false
-          moves += 1
-          echo("Please Choose a Number.")
-          continue
-
-      try:
-          if grid[row][col] != $slot:
-            draw = false
-            moves += 1
-
-            echo("Slot taken. Choose another one")
-            continue
-      except:
-          draw = false
-          moves += 1
-
-          echo("Please Choose A Number 1-9.")
-          continue  
-
-      case xo
-      of "X": xo = "\e[31mX\e[m"
-      of "O": xo = "\e[32mO\e[m"
+      var ptrArray = [addr(slot), addr(row), addr(col)]
+      playerMove(ptrArray, addr(draw), addr(turns), addr(grid))
+      xoPrint(ptrArray, addr(xo), addr(grid))
+      aiMove()
 
       if aiEnabled:
         aiTurn = true
-
-      grid[row][col] = xo
 
   drawGrid(grid)
   echo("Game over!")
